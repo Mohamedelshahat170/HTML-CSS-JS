dcb828c2dd7e353578d830d6193b2242e32ade8d
Mohamedelshahat170 <mohamed99elshahat@gmail.com>
2021-02-07 19:07:56 +0300

    HTML CSS JS

---
+++ js/file.js
@@ -0,0 +1,19 @@
+// open function from button
+function openpopup(){
+    document.getElementById('header').style.display='block';
+}
+// close function (js event ===> conmment) button
+function closePopup(){
+    document.getElementById('header').style.display='none';
+}
+
+// nice scroll
+$("body").niceScroll({
+    cursorcolor:"#888",
+    cursorwidth:"10px",
+    cursorborder:"0",
+    cursorwidth:"10px",
+    cursorborderradius:"0",
+    zindex:"1"
+  });
+
